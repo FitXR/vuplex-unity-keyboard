5eba448ee624a02c6d13454aad71f9b2a40bfb78
Alex Vlasov <alex.vlasov@fitxr.com>
2024-07-19 16:57:13 +0200

    aligned font, colors and styles of the keyboard with the Figma

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Keyboard from './components/Keyboard';
+import './fonts.css';
 
 ReactDOM.render(<Keyboard/>, document.getElementById('root'));
