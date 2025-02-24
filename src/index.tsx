4b1474440e5a82c7e35449ce75c578164d3a00b1
Carlos Melero <carlos.melero@fitxr.com>
2025-02-24 17:11:12 +0000

    Fix unused import

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Keyboard from './components/Keyboard';
-import './fonts.css';
 
 ReactDOM.render(<Keyboard/>, document.getElementById('root'));
